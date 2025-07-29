53b801674a50d7e1b769066e799905b3893ac650
Alaina Patel <alainaapatel@gmail.com>
2025-07-29 14:05:17 -0700

    dont w jointhing and like the other thing

---
+++ Connext/joinProject.swift
@@ -0,0 +1,59 @@
+//
+//  joinProject.swift
+//  Connext
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct joinProject: View {
+    @State private var name = "Alaina Patel"
+    @State private var projectName = "myProject"
+    
+    var body: some View {
+        ZStack {
+            Color(.beige)
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Text("Join a Project")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding()
+                        .padding()
+                    
+                    Spacer()
+                }
+                
+                Image("listingBackround")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                
+                Spacer()
+            } // VStack closing
+            
+            Image("toolbar")
+                .resizable()
+                .ignoresSafeArea()
+                .padding(.top, 0.0)
+
+            VStack {
+                NavigationLink(destination: ContentView()) {
+                    Image("houseIcon")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .frame(width: 50, height: 38)
+                        .padding(.top, 730.0)
+                }
+            }
+        } // ZStack closing
+    } // body closing
+} // struct closing
+
+#Preview {
+    joinProject()
+}
