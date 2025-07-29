69f869d6789a3b4b1f149fc927609a8f08ce5060
Alisa Kyrychenko <alisakyry726@gmail.com>
2025-07-29 11:03:17 -0700

    added two files and started fresh on home page

--- Connext/ContentView.swift
+++ Connext/ContentView.swift
@@ -9,17 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Change 1: Person A(Alisa")
-            Text("Change 2: Person B(Michelle)")
-            Text ("Change 3: Person C(Alaina)")
-            Text("New change - alisa")
-        }
-        .padding()
+        
     }
 }
 
---
+++ Connext/createProject.swift
@@ -0,0 +1,18 @@
+//
+//  createProject.swift
+//  Connext
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct createProject: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    createProject()
+}
