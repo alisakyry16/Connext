5b6b83f4ee2ace04296ecbf68900ae36505a0601
Alisa Kyrychenko <alisakyry726@gmail.com>
2025-07-29 10:23:54 -0700

    Initial Commit

---
+++ Connext/ConnextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnextApp.swift
+//  Connext
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ConnextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
