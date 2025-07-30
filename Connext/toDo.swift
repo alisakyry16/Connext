180c653c7e6486f81ea9f325288da14c27169236
Alaina Patel <alainaapatel@gmail.com>
2025-07-30 13:27:52 -0700

    todolist work in progress but most recent as of july30th right before alisa leaves

--- Connext/toDo.swift
+++ Connext/toDo.swift
@@ -1,49 +1,74 @@
 //
-//  toDo.swift
-//  Connext
+//  ContentView.swift
+//  ToDoList
 //
-//  Created by Scholar on 7/30/25.
+//  Created by Scholar on 7/25/25.
 //
 
 import SwiftUI
+import SwiftData
 
 struct toDo: View {
-    @State private var userToDo: String = ""
-    var body: some View {
-        NavigationStack {
-            ZStack {
-                Color(.beige)
-                    .ignoresSafeArea()
-                Text(userToDo)
-                VStack {
-                    HStack {
-                        Text("To-Do's")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .padding()
-                        
-                        Spacer()
-                    }//HStack Closing
-                   Spacer()
-                    HStack{
-                        Text("Add your To-Do")
-                            .padding()
-                            .font(.title3)
-                            .fontWeight(.bold)
-                        TextField("Type here", text: $userToDo)
-                            .font(.title3)
-                    }
-                
-                    
-                }
-                
+    @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
+    @State private var showNewTask = false
 
-            }//ZStack closing
-            customToolbar()
+    var body: some View {
+        ZStack {
+            Color(.beige).ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Text("To Do List")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Button {
+                        withAnimation {
+                            showNewTask = true
+                        }
+                    } label: {
+                        Text("+")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                    }
+                }
+                .padding()
+                
+                Spacer()
+                
+                List {
+                    ForEach(toDos) { toDoItem in
+                        if toDoItem.isImportant {
+                            Text("‼️ " + toDoItem.title)
+                        } else {
+                            Text(toDoItem.title)
+                        }
+                    }
+                    .onDelete(perform: deleteToDo)
+                }
+                .listStyle(.plain)
+                .background(Color.clear)
+            }
+            
+            if showNewTask {
+                NewToDoView(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
+            }
+        }
+    }
+    
+    // ✅ Moved OUTSIDE body
+    func deleteToDo(at offsets: IndexSet) {
+        for offset in offsets {
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
         }
     }
 }
 
 #Preview {
     toDo()
+        .modelContainer(for: ToDoItem.self, inMemory: true)
 }
